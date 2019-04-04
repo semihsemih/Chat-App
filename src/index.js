dca090192c04c103b08aa4a8ce487997c00946ca
Semih Arslan <semihsemih@me.com>
2019-04-04 16:17:01 +0300

    Rendering user list on sidebar

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ io.on('connection', (socket) => {
 
     socket.emit('message', generateMessage('Admin', 'Welcome!'))
     socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
 
     callback()
   })
@@ -59,6 +63,10 @@ io.on('connection', (socket) => {
 
     if (user) {
       io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
     }
   })
 
